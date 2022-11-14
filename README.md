e973b5f51084de79f89420781e7b7ca7633bfee3
Andres Bojaca <andresbojaca742@gmail.com>
2022-11-14 15:39:20 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# API RESTFULL 
+##### Prueba técnica Double V Partners
+#
+Requerimientos: 
+Queremos un API que nos permita crear, eliminar, editar y recuperar tickets con paginación. Que se pueda recuperar todos o filtrar por uno específico. Los ticket tienen un id, un usuario, una fecha de creación, una fecha de actualización y un estatus (abierto/cerrado).
+
+Exponer el servicio mediante http RESTFUL, valorable GRPC y GraphQL.
+Debes realizar la prueba en PHP
+Dejar en el readme la manera de poder probar en local. Utiliza el lenguaje de
+programación (PHP). Tener en cuenta, para la ejecución en local, la base
+
+Referencia : https://drive.google.com/file/d/1NmVi4IlpkQE3J4iJO-VL7k_7ybpuyyJP/view
+
+Esta prueba fue desarollada en su totalidad por @AndresBojaca con un tiempo estimado de 24h, fue iniciada el día Domingo
+13/11/2022 y terminada el dia Lunes.
+
+## Tecnologías Empleadas
+- [PHP 8.0.1](https://www.php.net/manual/es/intro-whatis.php)- Lenguaje Base utilizado para el core de la aplicacion
+- [Slim Framework v4](http://dev.slimframework.com/)- Microframework de PHP utilizado para la elaboracion del API 
+- [MySQL](https://www.mysql.com/) - Gestor de la BD
+- [Docker](https://www.docker.com/) - Contenedor para correr la BD
+## Instalación
+
+Clonar el repositorio de Github desde una terminal o bash
+
+```sh
+git clone https://github.com/AndresBojaca/api-restfull-doublevpartners.git
+cd api-restfull-doublevpartners
+```
+
+Levantar el Servidor de PHP
+
+```sh
+php -S localhost:8888
+```
+
+En el caso de usar un servidor local como XAMPP/WAMP, mover a la carpeta /www o cloner directamente en la ruta:
+```sh
+cd C:/wamp64/www
+git clone https://github.com/AndresBojaca/api-restfull-doublevpartners.git
+cd api-restfull-doublevpartners
+```
+## Docker
+
+Dillinger is very easy to install and deploy in a Docker container.
+
+By default, the Docker will expose port 8080, so change this within the
+Dockerfile if necessary. When ready, simply use the Dockerfile to
+build the image.
+
+```sh
+cd dillinger
+docker build -t <youruser>/dillinger:${package.json.version} .
+```
+
+This will create the dillinger image and pull in the necessary dependencies.
+Be sure to swap out `${package.json.version}` with the actual
+version of Dillinger.
+
+Once done, run the Docker image and map the port to whatever you wish on
+your host. In this example, we simply map port 8000 of the host to
+port 8080 of the Docker (or whatever port was exposed in the Dockerfile):
+
+```sh
+docker run -d -p 8000:8080 --restart=always --cap-add=SYS_ADMIN --name=dillinger <youruser>/dillinger:${package.json.version}
+```
+
+> Note: `--capt-add=SYS-ADMIN` is required for PDF rendering.
+
+Verify the deployment by navigating to your server address in
+your preferred browser.
+
+```sh
+127.0.0.1:8000
+```
+
+
+**Gracias por la oportunidad!**
---
+++ public/index.php
@@ -0,0 +1,262 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Log\LoggerInterface;
+use Slim\Factory\AppFactory;
+
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config/db.php';
+
+$app = AppFactory::create();
+
+// Middlewares
+$app->addRoutingMiddleware();
+$app->addBodyParsingMiddleware();
+
+// Redireccionamiento de Rutas
+$app->redirect('/', '/api/');
+$app->redirect('/api/tickets', '/api/tickets/all');
+$app->redirect('/api/ticket', '/api/tickets/all');
+
+// Monitoreo de Errores
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// Ruta Principal
+$app->get('/api/', function (Request $request, Response $response, $args) {
+    // Estructura para el response
+    $json = array(
+        "endpoints" => [
+            "tickets" => "http://doublevpartnersapi.local/tickets/all"
+        ],
+    );
+    $response->getBody()->write(json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+    return $response
+        ->withHeader('Content-type', 'application/json')
+        ->withStatus(200);
+});
+
+// Tickets Routes
+
+// Obtener todos los Tickets
+$app->get('/api/tickets/all', function (Request $request, Response $response) {
+
+    // Paginación
+    $data = $request->getQueryParams();
+    $page = (isset($data['page']) && $data['page'] > 0) ? $data['page'] : 1; // Default 1
+
+    // Limite Máximo de Registros
+    if (isset($data['limit'])) {
+        // Si viene $limit en la URL
+        $maxlimit = intval($data['limit']);
+        if ($data['limit'] > 100) {
+            // Si $limit es mayor a 100 deja maximo 100 registros
+            $maxlimit = 100;
+        }
+    } else {
+        // Si no viene $limit en la URL el default es 5
+        $maxlimit = $data['limit'] = 5;
+    }
+    // Limite de Registros
+    $limit = $maxlimit;
+    $offset = (--$page) * $limit; // Default 1
+
+    $sql = "
+    SELECT * FROM tickets WHERE status != 'Cerrado' LIMIT $limit OFFSET $offset";
+
+    try {
+
+        $db = new DB();
+        $conn = $db->connect();
+        $stmt = $conn->query($sql);
+        $tickets = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $db = null;
+
+        if ($stmt->rowCount() > 0) {
+            // Estructura para el response
+            $json = array(
+                "total_tickets" => $stmt->rowCount(),
+                "limit" => $limit,
+                "page" => $page + 1,
+                "tickets" => $tickets
+            );
+            $response->getBody()->write(json_encode($json, JSON_PRETTY_PRINT));
+        } else {
+            $response->getBody()->write('No Se han Encontrado Tickets');
+        }
+
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+// Obtener un solo ticket
+$app->get('/api/tickets/{id}', function (Request $request, Response $response, array $args) {
+
+    $id = isset($args['id']);
+    $sql = "
+    SELECT * FROM tickets WHERE id = $id AND status != 0";
+    try {
+
+        $db = new DB();
+        $conn = $db->connect();
+        $stmt = $conn->query($sql);
+        $ticket = $stmt->fetch(PDO::FETCH_ASSOC);
+        $db = null;
+
+        if ($stmt->rowCount() > 0) {
+            $response->getBody()->write(json_encode($ticket, JSON_PRETTY_PRINT));
+        } else {
+            $response->getBody()->write('No Se ha Encontrado ese Ticket en Específico');
+        }
+
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+// Insertar un Ticket
+$app->post('/api/tickets/add', function (Request $request, Response $response) {
+
+
+    $data = $request->getParsedBody();
+
+    // Obtenermos Parametros del Body JSON
+    $user = $data['user'];
+    $create_date = date('Y-m-d');
+    $update_date = date('Y-m-d');
+    $status = $data['status'];
+
+    $sql = "
+    INSERT INTO tickets (user, create_date, update_date, status) VALUES (:user, :create_date, :update_date, :status);";
+    try {
+
+        $db = new DB();
+        $conn = $db->connect();
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':user', $user);
+        $stmt->bindParam(':create_date', $create_date);
+        $stmt->bindParam(':update_date', $update_date);
+        $stmt->bindParam(':status', $status);
+
+        $result = $stmt->execute();
+
+        $db = null;
+        $response->getBody()->write("Ticket Creado con Éxito!");
+
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+// Editar un Ticket
+$app->put('/api/tickets/{id}', function (Request $request, Response $response, array $args): Response {
+
+
+    $data = $request->getParsedBody();
+    $id = $args['id'];
+
+    // Obtenermos Parametros del Body JSON
+    $user = $data['user'];
+    $update_date = "2022-12-10";
+    $status = $data['status'];
+
+    $sql = "
+    UPDATE tickets SET 
+    user = :user,
+    update_date = :update_date,
+    status = :status
+    WHERE id = $id";
+    try {
+
+        $db = new DB();
+        $conn = $db->connect();
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':user', $user);
+        $stmt->bindParam(':update_date', $update_date);
+        $stmt->bindParam(':status', $status);
+
+        $result = $stmt->execute();
+
+        $db = null;
+        $response->getBody()->write('Ticket #' . $id . ' Modificado con Éxito!');
+
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+// Eliminar un Ticket
+$app->delete('/api/tickets/delete/{id}', function (Request $request, Response $response, array $args) {
+
+    $id = $args['id'];
+    $sql = "
+    UPDATE tickets SET status = 0 WHERE id = $id";
+    try {
+
+        $db = new DB();
+        $conn = $db->connect();
+        $stmt = $conn->query($sql);
+        $db = null;
+
+        $response->getBody()->write('Ticket #' . $id . ' Eliminado con Éxito!');
+
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+
+// Run app
+$app->run();
